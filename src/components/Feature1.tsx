a0a0c789d9a71c1f95c15cb523557b006185ae43
Omkar Prajapati <prajapatiomkar9833@gmail.com>
2025-04-12 21:44:35 +0530

    added the section in the feature section.

--- src/components/Feature1.tsx
+++ src/components/Feature1.tsx
@@ -14,6 +14,38 @@ export default function Feature1() {
         <p>Compare all Pro features</p>
         <ArrowRight />
       </div>
+      <div className="flex flex-col lg:flex-row lg:items-center mt-10">
+        <div className="flex flex-col items-center text-center p-4 ">
+          <div className="w-15 h-15 flex items-center justify-center rounded-full bg-[#F4EFF6] mb-3">
+            <img src="earth.svg" alt="" />
+          </div>
+          <p className="font-medium text-black">Globally Accepted</p>
+          <p className="text-gray-500 text-sm mt-3 m-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center p-4">
+          <div className="w-15 h-15 flex items-center justify-center rounded-full bg-[#F4EFF6] mb-3">
+            <img src="key.svg" alt="" />
+          </div>
+          <p className="font-medium text-black">Biometric Integrated</p>
+          <p className="text-gray-500 text-sm mt-3 m-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+        <div className="flex flex-col items-center text-center p-4">
+          <div className="w-15 h-15 flex items-center justify-center rounded-full bg-[#F4EFF6] mb-3">
+            <img src="lock.svg" alt="" />
+          </div>
+          <p className="font-medium text-black">Fully Secured</p>
+          <p className="text-gray-500 text-sm mt-3 m-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
